8d7d25e51965d3195f64a7bb66d4dcc1a39794f2
Linton Ye <linton@jimulabs.com>
2018-03-20 17:02:40 -0700

    Bigger domo and hat

--- domo-hat-shop/app/Domo.js
+++ domo-hat-shop/app/Domo.js
@@ -1,38 +1,38 @@
 import React, { PureComponent } from "react";
-import { View, Image, Animated } from "react-native";
+import { View, Image, Animated, Dimensions } from "react-native";
 import DomoPng from "./images/domo.png";
 import styled from "styled-components";
 import Hat from "./Hat";
 
 const StyledHat = styled(Animated.createAnimatedComponent(Hat))`
   position: absolute;
-  left: 150px;
-  top: -305px;
-  width: 120px;
-  height: 120px;
+  left: 50%;
+  top: -610px;
+  width: ${props => props.size || 120}px;
+  height: ${props => props.size || 120}px;
 `;
 
 const DomoImage = styled.Image`
-  width: 300px;
-  height: 300px;
+  width: ${props => props.size || 200}px;
+  height: ${props => props.size || 200}px;
 `;
 
 /**
  * -1 - 0: hat moving to the left
  * 0  - 1: hat moving to the right
  */
-const AnimatedHat = ({ position, type }) => {
+const AnimatedHat = ({ position, type, size }) => {
   const rotate = position.interpolate({
     inputRange: [-1, 0, 1],
     outputRange: ["-360deg", `0deg`, `360deg`]
   });
   const translateX = position.interpolate({
     inputRange: [-1, 0, 1],
-    outputRange: [-200, 0, 200]
+    outputRange: [-2 * size, 0, 2 * size]
   });
   const translateY = position.interpolate({
     inputRange: [-1, 0, 1],
-    outputRange: [-50, 0, -50]
+    outputRange: [-0.8 * size, 0, -0.8 * size]
   });
   const opacity = position.interpolate({
     inputRange: [-1, -0.6, 0, 0.6, 1],
@@ -41,6 +41,7 @@ const AnimatedHat = ({ position, type }) => {
   return (
     <StyledHat
       type={type}
+      size={size}
       style={{
         opacity,
         transform: [{ translateX }, { translateY }, { rotate }]
@@ -50,6 +51,20 @@ const AnimatedHat = ({ position, type }) => {
 };
 
 export default class Domo extends PureComponent {
+  state = {
+    windowSmallerSide: Math.min(
+      Dimensions.get("window").width,
+      Dimensions.get("window").height
+    )
+  };
+  dimensionHandler = ({ window, screen }) =>
+    this.setState({ windowSmallerSide: Math.min(window.width, window.height) });
+  componentDidMount = () => {
+    Dimensions.addEventListener("change", this.dimensionHandler);
+  };
+  componentWillUnmount = () => {
+    Dimensions.removeEventListener("change", this.dimensionHandler);
+  };
   render() {
     const { hatLeft, hat, hatRight, transitionProgress } = this.props;
     const hatLeftPosition = transitionProgress.interpolate({
@@ -64,13 +79,23 @@ export default class Domo extends PureComponent {
       inputRange: [-1, 0, 1],
       outputRange: [1, 1, 0]
     });
+    const domoSize = this.state.windowSmallerSide * 0.8;
+    const hatSize = this.state.windowSmallerSide * 0.3;
     return (
       <View>
-        <DomoImage source={DomoPng} />
+        <DomoImage source={DomoPng} size={domoSize} />
         <View>
-          <AnimatedHat type={hatLeft} position={hatLeftPosition} />
-          <AnimatedHat type={hatRight} position={hatRightPosition} />
-          <AnimatedHat type={hat} position={thisPosition} />
+          <AnimatedHat
+            type={hatLeft}
+            position={hatLeftPosition}
+            size={hatSize}
+          />
+          <AnimatedHat
+            type={hatRight}
+            position={hatRightPosition}
+            size={hatSize}
+          />
+          <AnimatedHat type={hat} position={thisPosition} size={hatSize} />
         </View>
       </View>
     );
