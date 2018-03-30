259e5817515ff706f2bd4cd971c9ecdeef270e29
Linton Ye <linton@jimulabs.com>
2018-03-29 17:52:37 -0700

    Screencast updates

--- domo-hat-shop/App.js
+++ domo-hat-shop/App.js
@@ -1,33 +1,38 @@
 import React from "react";
-import { Image, View, Text, ScrollView } from "react-native";
+import { Image, View, Text, FlatList } from "react-native";
 
-import domoImage from "./images/domo.png";
-import domoYogaImage from "./images/domo-yoga.png";
+import { manyHats } from "./Data";
+
+function renderItem(itemInfo) {
+  return <Image source={itemInfo.item.image} />;
+}
+
+const Separator = () => (
+  <View style={{ width: "100%", height: 1, backgroundColor: "#eee" }} />
+);
+
+const Header = () => (
+  <View style={{ height: 150, backgroundColor: "orange" }}>
+    <Text
+      style={{
+        fontSize: 40,
+        textAlign: "center",
+        color: "white",
+        marginTop: 60
+      }}
+    >
+      Domo's Hats
+    </Text>
+  </View>
+);
 
 const App = () => (
-  <ScrollView style={{ padding: 16 }} maximumZoomScale={5}>
-    <Image source={domoImage} />
-    <Text style={{ fontSize: 20 }}>
-      The UI built with React is
-      <Text style={{ fontStyle: "italic", color: "blue" }}> reactive</Text>. As
-      a developer, you just need to write down{" "}
-      <Text style={{ fontWeight: "900" }}>what you want</Text> and React figures
-      out <Text style={{ fontWeight: "900" }}>how to do it</Text>.
-    </Text>
-    <Text style={{ fontSize: 20, marginTop: 16 }}>
-      When data change, your UI changes accordingly. You don’t need to worry
-      about updating the DOM, React does it for you automatically. The idea of
-      reactive UI greatly simplifies UI development.
-    </Text>
-    <Image source={domoYogaImage} />
-    <Text style={{ fontSize: 20 }}>
-      So, DOM is a … tree? Yup a tree! Oddly enough, a lot of things in your
-      computer look like a tree1. Let’s give DOM a nickname… ummm what about
-      Domo? Domo works as a model at the studio “Web Browser”. His job is to
-      pose in front of the artist who paints a portrait (or perhaps millions of
-      portraits).
-    </Text>
-  </ScrollView>
+  <FlatList
+    data={manyHats}
+    renderItem={renderItem}
+    ItemSeparatorComponent={Separator}
+    ListHeaderComponent={Header}
+  />
 );
 
 export default App;
