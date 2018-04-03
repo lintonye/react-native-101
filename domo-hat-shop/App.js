83fcaed4e3e05be0b3626fc7dd1018e44e0d8792
Linton Ye <linton@jimulabs.com>
2018-04-03 10:34:26 -0700

    Initial code

--- domo-hat-shop/App.js
+++ domo-hat-shop/App.js
@@ -1,23 +1,11 @@
 import React from "react";
-import { Image, View, Text } from "react-native";
+import { Image, View } from "react-native";
 
 import domoImage from "./images/domo.png";
 
 const App = () => (
   <View style={{ padding: 16 }}>
     <Image source={domoImage} />
-    <Text style={{ fontSize: 20 }}>
-      The UI built with React is
-      <Text style={{ fontStyle: "italic", color: "blue" }}> reactive</Text>. As
-      a developer, you just need to write down{" "}
-      <Text style={{ fontWeight: "900" }}>what you want</Text> and React figures
-      out <Text style={{ fontWeight: "900" }}>how to do it</Text>.
-    </Text>
-    <Text style={{ fontSize: 20, marginTop: 16 }}>
-      When data change, your UI changes accordingly. You don’t need to worry
-      about updating the DOM, React does it for you automatically. The idea of
-      reactive UI greatly simplifies UI development.
-    </Text>
   </View>
 );
 
