e541ae07dbbd9902b01bc53b574e69f252184e77
Linton Ye <linton@jimulabs.com>
2018-04-03 14:36:10 -0700

    Screencast updates

--- domo-hat-shop/App.js
+++ domo-hat-shop/App.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { Image, View } from "react-native";
+import {
+  Image,
+  View,
+  Button,
+  TouchableNativeFeedback,
+  TouchableOpacity
+} from "react-native";
 
 import domoImage from "./images/tree_hold.png";
 import hatHarry from "./images/hat-harry.png";
@@ -9,6 +15,9 @@ class App extends React.Component {
   state = {
     hat: "harry"
   };
+  changeHat = () => {
+    this.setState({ hat: this.state.hat === "harry" ? "pirate" : "harry" });
+  };
   render() {
     const hatImg = this.state.hat === "harry" ? hatHarry : hatPirate;
     return (
@@ -17,16 +26,40 @@ class App extends React.Component {
           source={domoImage}
           style={{ width: 350, height: 350, marginTop: 60 }}
         />
-        <Image
-          source={hatImg}
+        <TouchableOpacity
+          onPress={this.changeHat}
           style={{
             position: "absolute",
             left: 130,
-            top: 38,
-            width: 120,
-            height: 120
+            top: 38
           }}
+        >
+          <Image
+            source={hatImg}
+            style={{
+              width: 120,
+              height: 120
+            }}
+          />
+        </TouchableOpacity>
+        <Button
+          title="Change to pirate hat"
+          onPress={() => this.setState({ hat: "pirate" })}
+          color="purple"
         />
+        <TouchableNativeFeedback
+          onPress={() => this.setState({ hat: "pirate" })}
+        >
+          <View>
+            <Image
+              source={hatPirate}
+              style={{
+                width: 120,
+                height: 120
+              }}
+            />
+          </View>
+        </TouchableNativeFeedback>
       </View>
     );
   }
