a7902e0d39c0e325e2d6b8ef598d4d05a1c9bec4
Linton Ye <linton@jimulabs.com>
2018-04-03 11:28:33 -0700

    Add tree with hat

--- domo-hat-shop/App.js
+++ domo-hat-shop/App.js
@@ -1,12 +1,35 @@
 import React from "react";
 import { Image, View } from "react-native";
 
-import domoImage from "./images/domo.png";
+import domoImage from "./images/tree_hold.png";
+import hatHarry from "./images/hat-harry.png";
+import hatPirate from "./images/hat-pirate.png";
 
-const App = () => (
-  <View style={{ padding: 16 }}>
-    <Image source={domoImage} />
-  </View>
-);
+class App extends React.Component {
+  state = {
+    hat: "harry"
+  };
+  render() {
+    const hatImg = this.state.hat === "harry" ? hatHarry : hatPirate;
+    return (
+      <View style={{ padding: 16 }}>
+        <Image
+          source={domoImage}
+          style={{ width: 350, height: 350, marginTop: 60 }}
+        />
+        <Image
+          source={hatImg}
+          style={{
+            position: "absolute",
+            left: 130,
+            top: 38,
+            width: 120,
+            height: 120
+          }}
+        />
+      </View>
+    );
+  }
+}
 
 export default App;
