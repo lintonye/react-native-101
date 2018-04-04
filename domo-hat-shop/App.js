b5425c2380e087bb294498cb12eab6fba346c26e
Linton Ye <linton@jimulabs.com>
2018-04-04 12:05:17 -0700

    Update app and domo image

--- domo-hat-shop/App.js
+++ domo-hat-shop/App.js
@@ -7,7 +7,7 @@ import {
   TouchableOpacity
 } from "react-native";
 
-import domoImage from "./images/tree_hold.png";
+import domoImage from "./images/tree_point.png";
 import hatHarry from "./images/hat-harry.png";
 import hatPirate from "./images/hat-pirate.png";
 
@@ -26,40 +26,21 @@ class App extends React.Component {
           source={domoImage}
           style={{ width: 350, height: 350, marginTop: 60 }}
         />
-        <TouchableOpacity
-          onPress={this.changeHat}
+        <Image
+          source={hatImg}
           style={{
             position: "absolute",
             left: 130,
-            top: 38
+            top: 38,
+            width: 120,
+            height: 120
           }}
-        >
-          <Image
-            source={hatImg}
-            style={{
-              width: 120,
-              height: 120
-            }}
-          />
-        </TouchableOpacity>
+        />
         <Button
           title="Change to pirate hat"
           onPress={() => this.setState({ hat: "pirate" })}
           color="purple"
         />
-        <TouchableNativeFeedback
-          onPress={() => this.setState({ hat: "pirate" })}
-        >
-          <View>
-            <Image
-              source={hatPirate}
-              style={{
-                width: 120,
-                height: 120
-              }}
-            />
-          </View>
-        </TouchableNativeFeedback>
       </View>
     );
   }
