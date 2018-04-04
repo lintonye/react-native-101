960e11f306dbddd6ad69c048f8578f2ff58a8613
Linton Ye <linton@jimulabs.com>
2018-04-04 12:35:37 -0700

    Screencast update

--- domo-hat-shop/App.js
+++ domo-hat-shop/App.js
@@ -4,12 +4,31 @@ import {
   View,
   Button,
   TouchableNativeFeedback,
-  TouchableOpacity
+  TouchableOpacity,
+  StyleSheet
 } from "react-native";
 
 import domoImage from "./images/tree_point.png";
 import hatHarry from "./images/hat-harry.png";
 import hatPirate from "./images/hat-pirate.png";
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 16
+  },
+  domo: {
+    width: 350,
+    height: 350,
+    marginTop: 60
+  },
+  hat: {
+    position: "absolute",
+    left: 130,
+    top: 38,
+    width: 120,
+    height: 120
+  }
+});
 
 class App extends React.Component {
   state = {
@@ -21,21 +40,9 @@ class App extends React.Component {
   render() {
     const hatImg = this.state.hat === "harry" ? hatHarry : hatPirate;
     return (
-      <View style={{ padding: 16 }}>
-        <Image
-          source={domoImage}
-          style={{ width: 350, height: 350, marginTop: 60 }}
-        />
-        <Image
-          source={hatImg}
-          style={{
-            position: "absolute",
-            left: 130,
-            top: 38,
-            width: 120,
-            height: 120
-          }}
-        />
+      <View style={styles.container}>
+        <Image source={domoImage} style={styles.domo} />
+        <Image source={hatImg} style={styles.hat} />
         <Button
           title="Change to pirate hat"
           onPress={() => this.setState({ hat: "pirate" })}
