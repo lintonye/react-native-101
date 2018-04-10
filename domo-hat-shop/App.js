0f833e76898cd4c5c7af095584ebda978760b1d8
Linton Ye <linton@jimulabs.com>
2018-04-10 11:09:02 -0700

    Starter code for simple layout

--- domo-hat-shop/App.js
+++ domo-hat-shop/App.js
@@ -1,46 +1,29 @@
 import React from "react";
-import { Image, View, StyleSheet } from "react-native";
+import { Image, View, ScrollView, Text, StyleSheet } from "react-native";
 
 import domoImage from "./images/tree_point.png";
 import hatHarry from "./images/hat-harry.png";
 import hatPirate from "./images/hat-pirate.png";
-import styled from "styled-components";
+import hatWinter from "./images/hat-winter.png";
+import RatingBar from "./RatingBar";
+import Price from "./Price";
 
-const Container = styled.View`
-  padding: 16px;
-`;
-
-const Domo = styled.Image`
-  width: 350px;
-  height: 350px;
-  margin-top: 60px;
-  background: ${props => props.background};
-`;
-
-const Hat = styled.Image`
-  position: absolute;
-  left: 130px;
-  top: 38px;
-  width: 120px;
-  height: 120px;
-`;
+const styles = StyleSheet.create({});
 
 class App extends React.Component {
-  state = {
-    hat: "harry"
-  };
-  changeHat = () => {
-    this.setState({ hat: this.state.hat === "harry" ? "pirate" : "harry" });
-  };
   render() {
-    const hatImg = this.state.hat === "harry" ? hatHarry : hatPirate;
-    return (
-      <Container>
-        <Domo source={domoImage} background="orange" />
-        <Hat source={hatImg} />
-      </Container>
-    );
+    return <View />;
   }
 }
 
 export default App;
+
+const hatDescription = `
+Let’s give DOM a nickname… ummm what about Domo? Domo works as a model at the studio “Web Browser”. His job is to pose in front of the artist who paints a portrait (or perhaps millions of portraits).
+
+The portraits are what we see in the web browser when visiting a website. A developer’s job is like that of a director who tells Domo what to wear and what pose to make. This determines what those portraits look like in the end. jQuery or React are known as libraries and they are the tools a developer uses to communicate with Domo.
+
+Let’s give DOM a nickname… ummm what about Domo? Domo works as a model at the studio “Web Browser”. His job is to pose in front of the artist who paints a portrait (or perhaps millions of portraits).
+
+The portraits are what we see in the web browser when visiting a website. A developer’s job is like that of a director who tells Domo what to wear and what pose to make. This determines what those portraits look like in the end. jQuery or React are known as libraries and they are the tools a developer uses to communicate with Domo.
+`;
