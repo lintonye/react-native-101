d727c3ad3ae077f14ca7da33fb65a7aa8c054277
Linton Ye <linton@jimulabs.com>
2018-04-10 16:55:23 -0700

    Add the end

--- domo-hat-shop/App.js
+++ domo-hat-shop/App.js
@@ -53,4 +53,6 @@ The portraits are what we see in the web browser when visiting a website. A deve
 Let’s give DOM a nickname… ummm what about Domo? Domo works as a model at the studio “Web Browser”. His job is to pose in front of the artist who paints a portrait (or perhaps millions of portraits).
 
 The portraits are what we see in the web browser when visiting a website. A developer’s job is like that of a director who tells Domo what to wear and what pose to make. This determines what those portraits look like in the end. jQuery or React are known as libraries and they are the tools a developer uses to communicate with Domo.
+
+<= The end
 `;
