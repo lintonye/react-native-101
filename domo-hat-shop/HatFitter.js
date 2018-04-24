9f11126377bcde4f9f414435966f55c7bed42bd2
Linton Ye <linton@jimulabs.com>
2018-04-24 16:09:25 -0700

    Initial code

---
+++ domo-hat-shop/HatFitter.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import { StyleSheet, View, Image, SafeAreaView } from "react-native";
+import DomoImg from "./images/domo-thinker.png";
+import { LinearGradient } from "expo";
+import { hats } from "./Data";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    justifyContent: "center"
+  },
+  domoContainer: {
+    paddingTop: 50,
+    alignSelf: "center"
+  },
+  domo: {
+    width: 350,
+    height: 240,
+    alignSelf: "center"
+  },
+  hat: {
+    width: 100,
+    height: 100
+  },
+  hatPos: {
+    position: "absolute",
+    left: 100,
+    top: -30
+  }
+});
+
+export default class HatFitter extends Component {
+  render() {
+    return (
+      <LinearGradient
+        colors={["#6ea849", "#c2dfc2", "#f2fff2"]}
+        style={styles.container}
+      >
+        <SafeAreaView>
+          <View style={styles.domoContainer}>
+            <Image source={DomoImg} style={styles.domo} />
+            <Image source={hats[0].image} style={[styles.hat, styles.hatPos]} />
+          </View>
+        </SafeAreaView>
+      </LinearGradient>
+    );
+  }
+}
