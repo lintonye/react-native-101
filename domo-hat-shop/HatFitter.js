bebe5b858a18788cc5a86a4afcc716a48644d668
Linton Ye <linton@jimulabs.com>
2018-04-26 12:16:55 -0700

    Screencast updates

--- domo-hat-shop/HatFitter.js
+++ domo-hat-shop/HatFitter.js
@@ -36,21 +36,49 @@ const styles = StyleSheet.create({
   }
 });
 
+function computeDistance(touch0, touch1) {
+  // distance = √((x0-x1)² + (y0-y1)²)
+  const sq = x => x * x;
+  return Math.sqrt(
+    sq(touch0.pageX - touch1.pageX) + sq(touch0.pageY - touch1.pageY)
+  );
+}
+
 export default class HatFitter extends Component {
   state = {
     translateX: 0,
-    translateY: 0
+    translateY: 0,
+    scale: 1
   };
   x0 = 0;
   y0 = 0;
   panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
     onMoveShouldSetPanResponder: () => true,
-    onPanResponderMove: (_, gestureState) => {
-      // set the state according to the touch position
-      this.setState({
-        translateX: this.x0 + gestureState.dx,
-        translateY: this.y0 + gestureState.dy
-      });
+    onPanResponderMove: (e, gestureState) => {
+      const { touches } = e.nativeEvent;
+      switch (touches.length) {
+        case 1:
+          // set the state according to the touch position
+          this.setState({
+            translateX: this.x0 + gestureState.dx,
+            translateY: this.y0 + gestureState.dy
+          });
+          break;
+        case 2:
+          this.setState({
+            scale:
+              computeDistance(touches[0], touches[1]) / this.baselineDistance
+          });
+      }
+    },
+    onPanResponderStart: e => {
+      const { touches } = e.nativeEvent;
+      if (touches.length === 2) {
+        // save the baseline distance
+        this.baselineDistance = computeDistance(touches[0], touches[1]);
+        console.log("baselineDistance", this.baselineDistance);
+      }
     },
     onPanResponderEnd: () => {
       // record the current translateX and Y
@@ -62,7 +90,8 @@ export default class HatFitter extends Component {
     const dragStyle = {
       transform: [
         { translateX: this.state.translateX },
-        { translateY: this.state.translateY }
+        { translateY: this.state.translateY },
+        { scale: this.state.scale }
       ]
     };
     return (
