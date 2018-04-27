b5a35c2b22b141b488c75229056e2828bd014ab2
Linton Ye <linton@jimulabs.com>
2018-04-27 10:28:30 -0700

    Only one hat

--- domo-hat-shop/HatSwitcher.js
+++ domo-hat-shop/HatSwitcher.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import { StyleSheet, View, Image, SafeAreaView, Button } from "react-native";
 import DomoImg from "./images/domo-thinker.png";
 import { LinearGradient } from "expo";
-import { hats } from "./Data";
+import harryPotterHat from "./images/hat_harry.png";
+import pirateHat from "./images/hat_pirate.png";
 
 const styles = StyleSheet.create({
   container: {
@@ -20,32 +21,25 @@ const styles = StyleSheet.create({
     alignSelf: "center"
   },
   hat: {
+    position: "absolute",
     width: 100,
     height: 100
-  },
-  hatPos: {
-    position: "absolute",
-    left: 130,
-    top: 0
   }
 });
 
 export default class HatSwitcher extends Component {
   state = {
-    currentHat: 0
+    hatLeft: 130,
+    hatTop: 0
   };
   changeHat = () => {
-    this.setState({ currentHat: this.state.currentHat === 1 ? 0 : 1 });
+    this.setState({ hatLeft: -50, hatTop: -50 });
   };
   render() {
     const { currentHat } = this.state;
-    const hat0Style = {
-      left: currentHat === 0 ? 130 : -60,
-      top: currentHat === 0 ? 0 : -100
-    };
-    const hat1Style = {
-      left: currentHat === 1 ? 130 : 300,
-      top: currentHat === 1 ? 0 : -100
+    const hatPositionStyle = {
+      left: this.state.hatLeft,
+      top: this.state.hatTop
     };
     return (
       <LinearGradient
@@ -56,12 +50,8 @@ export default class HatSwitcher extends Component {
           <View style={styles.domoContainer}>
             <Image source={DomoImg} style={styles.domo} />
             <Image
-              source={hats[0].image}
-              style={[styles.hat, styles.hatPos, hat0Style]}
-            />
-            <Image
-              source={hats[1].image}
-              style={[styles.hat, styles.hatPos, hat1Style]}
+              source={harryPotterHat}
+              style={[styles.hat, hatPositionStyle]}
             />
           </View>
           <Button title="Change Hat" onPress={this.changeHat} />
