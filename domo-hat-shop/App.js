9784fdc1c401578d4405073f8a153d0bc37c6cbc
Linton Ye <linton@jimulabs.com>
2018-05-23 16:19:04 -0700

    Screencast updates

--- domo-hat-shop/App.js
+++ domo-hat-shop/App.js
@@ -3,7 +3,19 @@ import HatList from "./HatList";
 import HatGrid from "./HatGrid";
 import HatDetail from "./HatDetail";
 import { manyHats } from "./Data";
+import { createStackNavigator } from "react-navigation";
+import { Button } from "react-native";
 
-const App = () => <HatList hats={manyHats} />;
+const HatListWithData = () => <HatList hats={manyHats} />;
+
+const App = createStackNavigator({
+  hatList: {
+    screen: HatListWithData,
+    navigationOptions: {
+      headerTitle: "Domo's Hat List",
+      headerRight: <Button title="Login" />
+    }
+  }
+});
 
 export default App;
