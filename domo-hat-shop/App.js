cca8462e6ae5d826a986d351a69bfd72670e50bb
Linton Ye <linton@jimulabs.com>
2018-05-24 19:19:54 -0700

    tabs solution

--- domo-hat-shop/App.js
+++ domo-hat-shop/App.js
@@ -2,9 +2,14 @@ import React from "react";
 import HatList from "./HatList";
 import HatGrid from "./HatGrid";
 import HatDetail from "./HatDetail";
+import MyAccount from "./MyAccount";
 import { manyHats } from "./Data";
-import { createStackNavigator } from "react-navigation";
-import { Button } from "react-native";
+import {
+  createStackNavigator,
+  createBottomTabNavigator
+} from "react-navigation";
+import { Button, SafeAreaView } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 const HatListWithData = ({ navigation }) => (
   <HatList
@@ -21,9 +26,38 @@ const HatDetailWithData = ({ navigation }) => {
   return <HatDetail hat={hatThatWasJustPressed} />;
 };
 
-const App = createStackNavigator({
+const Home = createBottomTabNavigator({
   hatList: {
     screen: HatListWithData,
+    navigationOptions: {
+      tabBarLabel: "Hats",
+      tabBarIcon: ({ focused, tintColor }) => (
+        <Ionicons
+          name={`ios-list${focused ? "" : "-outline"}`}
+          size={25}
+          color={tintColor}
+        />
+      )
+    }
+  },
+  myAccount: {
+    screen: MyAccount,
+    navigationOptions: {
+      tabBarLabel: "My Account",
+      tabBarIcon: ({ focused, tintColor }) => (
+        <Ionicons
+          name={`ios-person${focused ? "" : "-outline"}`}
+          size={25}
+          color={tintColor}
+        />
+      )
+    }
+  }
+});
+
+const App = createStackNavigator({
+  home: {
+    screen: Home,
     navigationOptions: {
       headerTitle: "Domo's Hat List"
     }
