7d221180a4c42b6b425e3863f03e594ef0e5264c
Linton Ye <linton@jimulabs.com>
2018-06-01 18:13:52 -0700

    Add hat images

--- domo-hat-shop/App.js
+++ domo-hat-shop/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Image, View, Text } from "react-native";
 
 import domoImage from "./images/domo.png";
+import hatImage from "./images/hat-pirate.png";
 
 const App = () => (
   <View style={{ padding: 16 }}>
@@ -13,6 +14,14 @@ const App = () => (
       <Text style={{ fontWeight: "900" }}>what you want</Text> and React figures
       out <Text style={{ fontWeight: "900" }}>how to do it</Text>.
     </Text>
+    <Image source={hatImage} style={{ width: 200, height: 200 }} />
+    <Image
+      source={{
+        uri:
+          "https://cdn.glitch.com/1fb3273a-81cb-45bc-acbd-555546cb098f%2Fhat_chef.png?1527901868766"
+      }}
+      style={{ width: 100, height: 100 }}
+    />
     <Text style={{ fontSize: 20, marginTop: 16 }}>
       When data change, your UI changes accordingly. You don’t need to worry
       about updating the DOM, React does it for you automatically. The idea of
